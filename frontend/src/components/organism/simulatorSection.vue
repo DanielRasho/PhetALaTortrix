595ff14aface90e3463d82e0a591067b65739095
Daniel Rayo <danirayoroldan@gmail.com>
2023-08-26 18:09:54 -0600

    Feat: Create simulatorSection component to contain each figure simulation..

---
+++ frontend/src/components/organism/simulatorSection.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div class="canvas">a</div>
+
+        <div class="params-box">
+            <fieldSection title="Este es un titulo" class="common-box">
+                <numberField name="Carga" placeholder="- - -" unit="nC" />
+            </fieldSection>
+            <fieldSection title="Este es un titulo" class="specific-box">
+                <numberField name="Carga" placeholder="- - -" unit="nC" />
+            </fieldSection>
+            <fieldSection title="Este es un titulo" class="submit-box">
+                <buttonImportant> ara ara </buttonImportant>
+            </fieldSection>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import buttonImportant from '@/components/atoms/buttonImportant.vue'
+import numberField from '@/components/atoms/numberField.vue'
+import fieldSection from '@/components/molecules/fieldSection.vue'
+</script>
+
+<style scoped>
+.canvas {
+    display: block;
+    background-color: rgb(207, 207, 207);
+    border-radius: 7px;
+    min-height: 55vh;
+    margin-bottom: 3ch;
+}
+.params-box {
+    width: 100%;
+    min-height: 25vh;
+    display: flex;
+    justify-content: space-around;
+}
+
+.params-box > div {
+    height: inherit;
+    padding: 0 3ch;
+}
+
+.params-box > .common-box {
+    border-right: 1px solid #ccc;
+    width: 25%;
+}
+.params-box > .specific-box {
+    width: 50%;
+}
+.params-box > .submit-box {
+    border-left: 1px solid #ccc;
+    width: 25%;
+}
+</style>
