54c79fdc96817757c53c977f075bcebc18159a1f
ElrohirGT <elrohirgt@gmail.com>
2023-08-27 09:08:41 -0600

    feat: Added Hemisphere drawing

--- frontend/src/views/Hemisphere.vue
+++ frontend/src/views/Hemisphere.vue
@@ -1,18 +1,21 @@
 <template>
     <main>
-        <canvas></canvas>
-        <fieldsContainer></fieldsContainer>
+        <div class="canvas" id="canvas"></div>
+        <fieldsContainer :params="fields" @hot-reload="hotReloading()"></fieldsContainer>
     </main>
 </template>
 
 <script setup>
 import fieldsContainer from '@/components/organism/fieldsContainer.vue'
-import { ref } from 'vue'
+import Two from 'two.js';
+import { onMounted, ref } from 'vue'
+import { initializePlane } from "../lib/plane"
+import { HALF_PI } from 'two.js/src/utils/math';
 
 const fields = ref({
     axis: {
-        x: [-5, 5],
-        y: [-5, 5]
+        x: [-5, 25],
+        y: [-5, 25]
     },
     figure: {
         radius: {
@@ -20,10 +23,69 @@ const fields = ref({
             name: 'Radius',
             unit: 'm'
         },
-        Charge: 34
+        Charge: {
+            value: 64,
+            name: 'Charge',
+            unit: "nC"
+        }
     },
     points: []
+});
+
+/**
+ * Draws the hemisphere into the screen.
+ * @param {Two} drawer TwoJS object
+ * @param {Number} columns number of columns of x-axis
+ * @returns {[Two.Text, Two.Text]} the text elements that display info about the hemisphere
+ */
+const drawHemisphere = (drawer, columns) => {
+    let columnGap = drawer.width / columns;
+
+    let originPos = [columnGap*3 - columnGap/2, drawer.height / 2];
+
+    let arcRadius = 100;
+    let figureColor = "blue";
+    let arc = drawer.makeArcSegment(originPos[0],originPos[1], arcRadius, arcRadius, HALF_PI, HALF_PI * 3);
+    arc.stroke = figureColor;
+    arc.linewidth = 3;
+
+    let line = drawer.makeLine(originPos[0], originPos[1] - arcRadius, originPos[0], originPos[1] + arcRadius);
+    line.stroke = figureColor;
+    line.linewidth = 3;
+
+    let circle = drawer.makeCircle(originPos[0] - arcRadius / 2, originPos[1], arcRadius / 2);
+    circle.scale = new Two.Vector(1, 0.4);
+    circle.stroke = figureColor;
+    circle.linewidth = 4;
+    circle.dashes = [4,5];
+    circle.fill = "transparent";
+
+    let rText = drawer.makeText("R = 0 m", originPos[0] + 5, originPos[1] - arcRadius, {
+        alignment: "left"
+    });
+    let cText = drawer.makeText("Q = 0 nC", originPos[0] - arcRadius / 2, originPos[1] + arcRadius / 4, {
+        fill: "red",
+        stroke: 10,
+    });
+
+    return [rText, cText];
+};
+
+let radiusText;
+let chargeText;
+
+onMounted(() => {
+    let elem = document.querySelector("#canvas");
+    let two = new Two({ fullscreen: false, width: elem.offsetWidth, height: elem.offsetHeight }).appendTo(elem);
+    let rows = 11;
+    let columns = 25;
+
+    initializePlane(two, columns, rows);
+    [radiusText, chargeText] = drawHemisphere(two, columns, rows);
+
+    two.update();
 })
+
 </script>
 <style scoped>
 main {
@@ -32,7 +94,7 @@ main {
 }
 .canvas {
     display: block;
-    background-color: rgb(207, 207, 207);
+    background-color: white;
     border-radius: 7px;
     min-height: 55vh;
     margin-bottom: 3ch;
