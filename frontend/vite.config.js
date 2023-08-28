b397054e07564141911a4af11b5d832f462e365b
ElrohirGT <elrohirgt@gmail.com>
2023-08-28 17:01:46 -0600

    feat: Added configs for autocompiling backend
    This dependencies autocompile the backend and add it to the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    wasm(),
-    topLevelAwait()
+    wasmPack('../tphet_core/')
   ],
   resolve: {
     alias: {
