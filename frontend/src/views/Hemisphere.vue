77f12d67c459847d16971f5b9fe605fd2757e312
ElrohirGT <elrohirgt@gmail.com>
2023-08-28 20:44:11 -0600

    fix: draw sphere finalized

--- frontend/src/views/Hemisphere.vue
+++ frontend/src/views/Hemisphere.vue
@@ -13,16 +13,17 @@ import fieldsContainer from '@/components/organism/fieldsContainer.vue'
 import { onMounted, ref, toRaw } from 'vue';
 import Two from 'two.js';
 import { HALF_PI } from 'two.js/src/utils/math';
-import { initializePlane } from '../lib/plane.js';
+import { initializePlane, drawPoints } from '../lib/plane.js';
 import init, { js_hemisphere_field_on } from 'tphet_core';
 
 let two;
 
-
 onMounted(async () => {
     await init();
+    
     let elem = document.querySelector("#canvas");
     two = new Two({ fullscreen: false, width: elem.offsetWidth, height: elem.offsetHeight }).appendTo(elem);
+
     drawCanvas(fields.value);
     two.update();
 });
@@ -88,49 +89,15 @@ const updateFields = (newValue) => {
     forwardArc.linewidth = 4;
     forwardArc.fill = "transparent";
 
-    let rString = `R = ${context.figure.radius.value} m`;
+    let rString = `R = ${context.figure.radius.value} ${context.figure.radius.unit}`;
     drawer.makeText(rString, originPos.x + 5, originPos.y - arcRadius, {
         alignment: "left"
     });
 
-    let cString = `Q = ${context.figure.charge.value} nC`
+    let cString = `Q = ${context.figure.charge.value} ${context.figure.charge.unit}`
     drawer.makeText(cString, originPos.x - arcRadius / 2, originPos.y + arcRadius / 4, {
         fill: "red",
         stroke: 10,
-    });
-};
-
-/**
- * Draws the hemisphere into the screen.
- * @param {Two} drawer TwoJS object
- * @param {Object} context object that contains all the fields that the user can change.
- */
-const drawPoints = (drawer, originPos, context) => {
-    let {min, max} = context.axis.x;
-    let virtualWidth = max-min;
-    let originY = drawer.height / 2;
-    let fig = {
-        radius: context.figure.radius.value,
-        charge: context.figure.charge.value,
-    };
-
-    let vectors = context.points.map((point) => [point, new Two.Vector(point / max * (drawer.width - originPos.x) + originPos.x, originY)]);
-    console.log("Points drawed");
-    vectors.forEach(([, v]) => {
-        let c = drawer.makeCircle(v.x, v.y, 4);
-        c.fill = "red";
-    });
-
-    vectors.forEach(([originalPoint, v]) => {
-        try {
-            let field = js_hemisphere_field_on(fig, originalPoint, 250);
-            console.log(`The field of (${originalPoint}, 0) is ${field}`);
-            let x = field / virtualWidth * drawer.width;
-            drawer.makeArrow(v.x, v.y, x, v.y);   
-        } catch (ex) {
-            console.log("Error creating arrows...");
-            console.error(ex);
-        }
     });
 };
 
@@ -142,7 +109,10 @@ function drawCanvas(context){
 
     initializePlane(two, columns, rows);
     drawHemisphere(two, originPos, context);
-    drawPoints(two, originPos, context);
+    drawPoints(two, originPos, context, (c) => ({
+        radius: c.figure.radius.value,
+        charge: c.figure.charge.value,
+    }), js_hemisphere_field_on);
 
     two.update();
 }
